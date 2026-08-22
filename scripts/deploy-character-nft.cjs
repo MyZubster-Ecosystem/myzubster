4a02cc4bfa33aab3224f03bd29382d4504e50ed9
Daniel Ioni <rossieugenio17@gmail.com>
2026-08-22 05:47:47 +0200

    feat: add Character NFT deploy script

---
+++ scripts/deploy-character-nft.cjs
@@ -0,0 +1,29 @@
+const hre = require('hardhat');
+
+async function main() {
+  const maxSupply = Number(process.env.CHARACTER_NFT_MAX_SUPPLY || 10000);
+  const [deployer] = await hre.ethers.getSigners();
+
+  if (!deployer) {
+    throw new Error('No deployer configured. Set EVM_DEPLOYER_PRIVATE_KEY.');
+  }
+
+  console.log('Deploying MyZubsterCharacter from:', deployer.address);
+  console.log('Max supply:', maxSupply);
+
+  const factory = await hre.ethers.getContractFactory('MyZubsterCharacter');
+  const contract = await factory.deploy(maxSupply, deployer.address);
+  await contract.waitForDeployment();
+
+  const address = await contract.getAddress();
+  const network = await hre.ethers.provider.getNetwork();
+
+  console.log('CHAIN_ID=' + network.chainId.toString());
+  console.log('MYZUBSTER_CHARACTER_CONTRACT=' + address);
+  console.log('NFT_CONTRACT_ALLOWLIST_' + network.chainId.toString() + '=' + address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
