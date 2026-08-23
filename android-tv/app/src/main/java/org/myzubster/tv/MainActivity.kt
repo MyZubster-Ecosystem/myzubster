84d334c505a85f484597be5c01599b5eccd1a43f
Daniel Ioni <rossieugenio17@gmail.com>
2026-08-23 11:38:01 +0200

    feat(tv): add remote-first WebView TV activity

---
+++ android-tv/app/src/main/java/org/myzubster/tv/MainActivity.kt
@@ -0,0 +1,52 @@
+package org.myzubster.tv
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this).apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = false
+            settings.allowFileAccess = false
+            settings.allowContentAccess = false
+            settings.setSupportZoom(false)
+            isFocusable = true
+            isFocusableInTouchMode = true
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+            systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                )
+        }
+
+        setContentView(webView)
+        webView.loadUrl(TV_URL)
+        webView.requestFocus()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    companion object {
+        private const val TV_URL = "https://myzubster.com/tv.html"
+    }
+}
