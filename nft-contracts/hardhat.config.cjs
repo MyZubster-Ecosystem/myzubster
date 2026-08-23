44ef19bdb554e07dfc64dd99e3fb3b3bab0a86b6
Daniel Ioni <rossieugenio17@gmail.com>
2026-08-23 13:36:49 +0200

    feat(nft): configure isolated hardhat project

---
+++ nft-contracts/hardhat.config.cjs
@@ -0,0 +1,10 @@
+require('@nomicfoundation/hardhat-ethers');
+
+module.exports = {
+  solidity: {
+    version: '0.8.24',
+    settings: {
+      optimizer: { enabled: true, runs: 200 }
+    }
+  }
+};
