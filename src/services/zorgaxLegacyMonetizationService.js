00e26fcfce8b8126e0a72940ee80354a6685dcd0
Daniel Ioni <danieldirimini@gmail.com>
2026-09-26 03:36:56 +0200

    revert: restore legacy monetization baseline

--- src/services/zorgaxLegacyMonetizationService.js
+++ src/services/zorgaxLegacyMonetizationService.js
@@ -5,23 +5,19 @@ const unified = require('./zorgaxUnifiedCheckoutService');
 
 const SUPPORTED_ASSETS = Object.freeze(['BTC']);
 const INTENT_TTL_MS = unified.INTENT_TTL_MS;
-function btcWallet() {
-  return typeof unified.btcWallet === 'function' ? unified.btcWallet() : '';
-}
-
-const DEFAULT_BTC_WALLET = btcWallet();
+const DEFAULT_BTC_WALLET = unified.btcWallet();
 
 function publicWallets() {
   return {
     ETH: '',
-    BTC: btcWallet(),
+    BTC: unified.btcWallet(),
     XMR: '',
     TARI: ''
   };
 }
 
 function isSettlementRailOperational(asset) {
-  return String(asset || '').toUpperCase() === 'BTC' && Boolean(btcWallet());
+  return String(asset || '').toUpperCase() === 'BTC' && Boolean(unified.btcWallet());
 }
 
 module.exports = {
