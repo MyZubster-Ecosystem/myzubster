504449d88f16c05047e57e38c3abae51383e53d5
Daniel Ioni <danieldirimini@gmail.com>
2026-09-26 03:43:24 +0200

    fix: break Zorgax checkout verifier dependency cycle

--- src/services/zorgaxChainVerifierService.js
+++ src/services/zorgaxChainVerifierService.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const crypto = require('crypto');
-const { SUPPORTED_ASSETS } = require('./zorgaxLegacyMonetizationService');
+const SUPPORTED_ASSETS = Object.freeze(['BTC']);
 
 const DEFAULT_BTC_ESPLORA_URL = 'https://blockstream.info/api';
 
